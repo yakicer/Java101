66101528a9ec643c5930e0f11cd65eccdf1cd797
yakicer <yakupicer@gmail.com>
2022-10-10 14:28:52 +0300

    Java Üçgen

---
+++ JavaUcgen.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class JavaUcgen {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int A,B,C;
+        double u,alan;
+        System.out.println("Dik Üçgenin Kenarlarını Giriniz.");
+        System.out.println("A kenarını giriniz:");
+        A = input.nextInt();
+        System.out.println("B kenarını giriniz:");
+        B = input.nextInt();
+        System.out.println("C kenarını giriniz:");
+        C = input.nextInt();
+        input.close();
+        u = (A+B+C)/2;
+        alan = Math.sqrt(u*(u-A)*(u-B)*(u-C));
+        System.out.println("Üçgenin Alanı = " +alan);
+    }
+}
